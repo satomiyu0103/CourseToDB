0c322daa8717d2451d16674b72b934f77eb234a1
yuro0103 <211184636+satomiyu0103@users.noreply.github.com>
2025-10-27 13:38:45 +0900

    fix: update environment loading to use project root path

--- main_py/20250702_create_attend_schedule.py
+++ main_py/20250702_create_attend_schedule.py
@@ -3,6 +3,7 @@
 表形式にリスト化し、参加講座の一覧を作る
 """
 
+from pathlib import Path
 from dotenv import load_dotenv
 import os
 
@@ -12,7 +13,7 @@ from modules.calendar_io import (
     input_file_to_df,
 )
 
-from modules.calendar_parser import creat_event_dic, creat_month_schdule
+# from modules.calendar_parser import creat_event_dic, creat_month_schdule
 from modules.schedule_output import (
     creat_schedule_df,
     attend_course_schedule_df,
@@ -21,7 +22,12 @@ from modules.schedule_output import (
 
 
 def main():
-    load_dotenv(r"main_py\.env")
+    try:
+        PROJECT_ROOT = Path(__file__).resolve().parent.parent
+    except NameError:
+        PROJECT_ROOT = Path.cwd()
+    env_path = PROJECT_ROOT / "main_py" / ".env"
+    load_dotenv(env_path)
     folda_path = os.getenv("FOLDA_PATH")
     data_list = creat_file_path_list(folda_path)
     file_path = data_list[-1]  # 最新のファイルを選択
